e4cf2b7c410e3247a5bd3bdecce91ff652569f13
Henrik <henrik789@gmail.com>
2019-04-08 14:51:58 +0200

    now with new animation

--- Tracker/ViewControllers/FlagViewController.swift
+++ Tracker/ViewControllers/FlagViewController.swift
@@ -3,23 +3,39 @@ import UIKit
 
 class FlagViewController: UIViewController {
     
-    @IBOutlet weak var flagImage: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
+    var aniImages: [UIImage] = []
     
-    let flagView = GetDataFlags()
-     var index = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Flag image show here."
+        self.title = "Animation show here."
+        
+        aniImages = createImageArray(total: 10, imagePrefix: "Idle")
         
     }
     
-    @IBAction func imageButton(_ sender: Any) {
-//        let nation = flagView.setNation(row: index)
-        flagImage.image = flagView.flagImageArray[index]
-        index += 1
-        if index == 20{
-            index = 0
+    func createImageArray(total: Int, imagePrefix: String) -> [UIImage]{
+        var imageArray: [UIImage] = []
+        
+        for imageCount in 1...total{
+            print( "\(imagePrefix)\(imageCount)")
+            let imageName = "\(imagePrefix)\(imageCount)"
+            let image = UIImage(named: imageName)!
+            
+            imageArray.append(image)
         }
+        return imageArray
+    }
+    
+    func animate(imageView: UIImageView, images: [UIImage]){
+        imageView.animationImages = images
+        imageView.animationDuration = 1.0
+        imageView.animationRepeatCount = 0
+        imageView.startAnimating()
+    }
+    
+    @IBAction func imageButton(_ sender: Any) {
+        animate(imageView: imageView, images: aniImages)
     }
 }
